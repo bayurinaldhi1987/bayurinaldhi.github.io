8b6faed09da51e933ad6d7ef76e252551c953f88
Bayu Rinaldhi <bayurinaldhi@gmail.com>
2025-02-12 21:06:54 +0700

    membuat website kedai kopi

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+// Toggle Class Active
+const navbarNav = document.querySelector ('.navbar-nav');
+
+
+// Ketika Hamburger Menu di Klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// Klik diluar sidebar untuk menghilangkan Nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active')
+    }
+})
